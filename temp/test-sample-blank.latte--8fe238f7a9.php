2dfc485d884c0a29661c50637dc905320232603b
Sukron Abbayroad <s77771983@gmail.com>
2024-12-22 00:38:02 +0700

    Membuat blank page

---
+++ temp/test-sample-blank.latte--8fe238f7a9.php
@@ -0,0 +1,84 @@
+<?php
+
+use Latte\Runtime as LR;
+
+/** source: D:\Sukron\My Tools\abiesoft-fullstack\vendor\abiesoft\Http/../../../templates/admin/page/test/sample/blank.latte */
+final class Template_8fe238f7a9 extends Latte\Runtime\Template
+{
+	public const Source = 'D:\\Sukron\\My Tools\\abiesoft-fullstack\\vendor\\abiesoft\\Http/../../../templates/admin/page/test/sample/blank.latte';
+
+	public const Blocks = [
+		['title' => 'blockTitle', 'content' => 'blockContent', 'js' => 'blockJs'],
+	];
+
+
+	public function main(array $ʟ_args): void
+	{
+		extract($ʟ_args);
+		unset($ʟ_args);
+
+		$this->renderBlock('title', get_defined_vars()) /* line 2 */;
+		echo "\n";
+		$this->renderBlock('content', get_defined_vars()) /* line 3 */;
+	}
+
+
+	public function prepare(): array
+	{
+		extract($this->params);
+
+		$this->parentName = '../../../main.latte';
+		return get_defined_vars();
+	}
+
+
+	/** {block title} on line 2 */
+	public function blockTitle(array $ʟ_args): void
+	{
+		extract($this->params);
+		extract($ʟ_args);
+		unset($ʟ_args);
+
+		echo LR\Filters::escapeHtmlText($title) /* line 2 */;
+	}
+
+
+	/** {block content} on line 3 */
+	public function blockContent(array $ʟ_args): void
+	{
+		extract($this->params);
+		extract($ʟ_args);
+		unset($ʟ_args);
+
+		echo '<div class=\'container\'>
+    <div class=\'empty\'>
+        <div class=\'center\'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <path fill-rule="evenodd" d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm18 3H3.75v9a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V9Zm-15-3.75A.75.75 0 0 0 4.5 6v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V6a.75.75 0 0 0-.75-.75H5.25Zm1.5.75a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V6Zm3-.75A.75.75 0 0 0 9 6v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V6a.75.75 0 0 0-.75-.75H9.75Z" clip-rule="evenodd"></path>
+            </svg>
+            <div class=\'title\'>Belum Ada Data</div>
+            <div class=\'des\'>
+                <p>Untuk sementara belum ada data di halaman ini.</p>
+                <p>Silahkan klik tambah data untuk membuat data pertama</p>
+            </div>
+            <div class=\'area-button\'>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd"></path>
+                    </svg>
+                    <span>Tambah Data</span>
+                </button>
+            </div>
+        <div>
+    </div>
+</div>
+';
+		$this->renderBlock('js', get_defined_vars()) /* line 26 */;
+	}
+
+
+	/** {block js} on line 26 */
+	public function blockJs(array $ʟ_args): void
+	{
+	}
+}
